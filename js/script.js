359f55c2a149470ce00348590f078c883d7fbd5b
btb0 <benburke2016@gmail.com>
2023-04-24 13:38:30 -0400

    constants and state vars

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 /*----- constants -----*/
-
+const suits = ['d', 'h', 'c', 's']
+const ranks = ['02', '03', '04', '05', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 /*----- state variables -----*/
-
+let playerHand
+let dealerHand
+let cardCountValue
+let turn
+let winner
 
 /*----- cached elements  -----*/
 
